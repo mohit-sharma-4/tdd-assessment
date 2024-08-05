7ee613bce02331fb747969982ab9d627dff2a204
Mohit Sharma <mohit@Mohits-MacBook-Air.local>
2024-08-05 18:35:40 +0530

    Add test for final result

--- src/assessment/add.test.js
+++ src/assessment/add.test.js
@@ -9,4 +9,9 @@ describe('Add string', () => {
   it('should show an error when using negative numbers', () => {
     expect(() => add('3;-1')).toThrow('negative numbers not allowed -1');
   });
+
+  it('should return sum of the string', () => {
+    const result = add('10;20//30\n40');
+    expect(result).toBe(100);
+  });
 });
