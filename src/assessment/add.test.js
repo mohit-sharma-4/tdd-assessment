8e94c8429b368e3cf13f7aef7bebe48290811320
Mohit Sharma <mohit@Mohits-MacBook-Air.local>
2024-08-05 18:30:28 +0530

    Add test for throwing an expection

--- src/assessment/add.test.js
+++ src/assessment/add.test.js
@@ -5,4 +5,8 @@ describe('Add string', () => {
     const result = add('');
     expect(result).toBe(0);
   });
+
+  it('should show an error when using negative numbers', () => {
+    expect(() => add('3;-1')).toThrow('negative numbers not allowed -1');
+  });
 });
